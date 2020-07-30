cf199a5a8169d6061d96aa6470ba24b5f3109437
kouul <maulloohans@gmail.com>
2020-07-30 15:33:18 +0400

    updated connection string to mongodb atlas

--- backend/app.py
+++ backend/app.py
@@ -4,13 +4,12 @@ import json
 
 app = Flask(__name__)
 
-mongoClient = MongoClient('mongodb://127.0.0.1:27017')
+mongoClient = MongoClient('mongodb+srv://<username>:<password>@cluster0-efer5b.mongodb.net/test?retryWrites=true&w=majority')
 db = mongoClient.get_database('fremp_test_app1_db')
 col = db.get_collection('fremp_test_app1_col')
 
-# $ mongo
-# $ use fremp_test_app1_db
-# $ db.fremp_test_app1_col.insertOne({'data': 'Hello World from MongoDB'})
+# Create an account on MongoDB Atlas
+# Add a JSON object to the collection
 
 @app.route('/api/get/')
 def getdata():
